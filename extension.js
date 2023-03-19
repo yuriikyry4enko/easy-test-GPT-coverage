6a34caf9878b587f6422b44e0a0473a1baa75268
Yurii_Kyrychenko@epam.com <yurii_kyrychenko@epam.com>
2023-03-19 23:32:05 +0200

    moved open-api-key into global config

--- extension.js
+++ extension.js
@@ -1,34 +1,39 @@
 const OpenAI = require('openai-api');
-// @ts-ignore
-const openai = new OpenAI('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');
 const vscode = require('vscode');
 
+const OPEN_AI_KEY_NAME = 'openai-api-key';
+
+const configuration = vscode.workspace.getConfiguration('easy-test-coverage-GPT');
 const model = 'text-davinci-002';
 const maxTokens = 2048;
-const basePrompt = "Create nUnit tests (only code without descuptions) with Mock if needed for this code: \n"
+const basePrompt = "Please write an nUnit test for the following code: \n"
+
+let openaiApiKey = '';
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	
+	openaiApiKey = configuration.get(OPEN_AI_KEY_NAME);
 
-	context.subscriptions.push(vscode.commands.registerCommand('easytestcoverage.copySelectionCommand', function () {
+	context.subscriptions.push(vscode.commands.registerCommand('easy-test-coverage-GPT.covertWithTestCommand', function () {
 		
 		const editor = vscode.window.activeTextEditor;
 
 		if (!editor) {
 		  return;
 		}
+
 		const selection = editor.selection;
 
 		if (selection.isEmpty) {
 		  return;
 		}
-		covertWithTest();
-	  }));
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('easytestcoverage.covertWithTestCommand', covertWithTest)
-	  );
+		covertWithTest();
+		
+	  }));
 }
 
 function covertWithTest() {
@@ -36,6 +41,11 @@ function covertWithTest() {
 	const selectedText = editorText.selection;
 	const text = editorText.document.getText(selectedText);
 	
+	checkApiKeyAvailable();
+
+	// @ts-ignore
+	const openai = new OpenAI(openaiApiKey);
+
 	openai.complete({
 		engine: model,
 		prompt: basePrompt + text,
@@ -55,6 +65,34 @@ function covertWithTest() {
 	  .catch(error => {
 		console.error(error);
 	  });	
+}
+
+function checkApiKeyAvailable(){
+	if (!openaiApiKey) {
+		// Prompt the user to enter their API key
+		vscode.window.showInputBox({
+			prompt: 'Please set your OpenAI API key',
+			value: openaiApiKey
+		}).then(apiKey => {
+			// Set the API key in the configuration
+			setApiKey(apiKey);
+			return;
+		});
+	}
+}
+
+function setApiKey(apiKey) {
+    configuration.update('openai-api-key', apiKey, vscode.ConfigurationTarget.Global)
+        .then(() => {
+            // Configuration parameter updated successfully
+            vscode.window.showInformationMessage('OpenAI API key has been updated.');
+			openaiApiKey = apiKey;
+        })
+        // @ts-ignore
+        .catch((error) => {
+            // Error updating configuration parameter
+            vscode.window.showErrorMessage('Failed to update OpenAI API key: ' + error.message);
+        });
 }
 
 // This method is called when your extension is deactivated
