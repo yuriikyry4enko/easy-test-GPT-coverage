f748fad13f2113e20e5e752c4efc8ab8c352d836
yuriikyry4nko <yuriykiri4enko@gmail.com>
2023-03-22 00:08:58 +0200

    added key snippets and refactored

--- extension.js
+++ extension.js
@@ -3,102 +3,116 @@ const vscode = require('vscode');
 
 const OPEN_AI_KEY_NAME = 'openai-api-key';
 
+let baseFrameWork = 'NUnit';
+let baseProgrammingLanguage = 'C#';
+let basePromptStart = `Create unit tests using ${baseFrameWork} framework for the ${baseProgrammingLanguage} C# method: \n`;
+let basePromptEnd = "\nProvide only code for copy without any additional info.";
+
 const configuration = vscode.workspace.getConfiguration('easy-test-coverage-GPT');
 const model = 'text-davinci-002';
 const maxTokens = 2048;
-const basePrompt = "Please write an nUnit test for the following code: \n"
 
+let additionalPromt = '';
 let openaiApiKey = '';
 
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
-	
-	openaiApiKey = configuration.get(OPEN_AI_KEY_NAME);
 
-	context.subscriptions.push(vscode.commands.registerCommand('easy-test-coverage-GPT.covertWithTestCommand', function () {
+	initializeSubscriptions(context);
+
+    openaiApiKey = configuration.get(OPEN_AI_KEY_NAME);
+}
+
+function initializeSubscriptions(context) {
+	context.subscriptions.push(
+        vscode.commands.registerCommand('easy-test-coverage-GPT.covertWithTestCommand', covertWithTest)
+    );
+
+	context.subscriptions.push(
+        vscode.commands.registerCommand('easy-test-coverage-GPT.changeChatGPTApiKeyCommand', changeChatGPTApiKey)
+    );
+}
+
+async function covertWithTest() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+    const selectedText = editor.selection;
+    if (selectedText.isEmpty) {
+        return;
+    }
+    const text = editor.document.getText(selectedText);
+
+    checkApiKeyAvailable();
+
+	vscode.window.withProgress({
+		location: vscode.ProgressLocation.Notification,
+		title: "Test coverage in progress..",
+		cancellable: false
+	}, async (progress, token) => {
+		// @ts-ignore
+		const openai = new OpenAI(openaiApiKey);
 		
-		const editor = vscode.window.activeTextEditor;
+		try {
+			const response = await openai.complete({
+				engine: model,
+				prompt: createPrompt(text, additionalPromt),
+				maxTokens: maxTokens
+			});
 
-		if (!editor) {
-		  return;
+			console.log(response.data.choices[0].text);
+			let responseText = response.data.choices[0].text + "\n";
+
+			const endPos = selectedText.end;
+			const newPosition = endPos.with(endPos.line + 1, 0);
+
+			editor.edit(editBuilder => {
+				editBuilder.insert(newPosition, responseText);
+			});
+		}catch (error) {
+			console.error(error);
+			vscode.window.showErrorMessage('Failed to generate test coverage');
 		}
-
-		const selection = editor.selection;
-
-		if (selection.isEmpty) {
-		  return;
-		}
-
-		covertWithTest();
-		
-	  }));
+	});
 }
 
-function covertWithTest() {
-	const editorText = vscode.window.activeTextEditor;
-	const selectedText = editorText.selection;
-	const text = editorText.document.getText(selectedText);
-	
-	checkApiKeyAvailable();
-
-	// @ts-ignore
-	const openai = new OpenAI(openaiApiKey);
-
-	openai.complete({
-		engine: model,
-		prompt: basePrompt + text,
-		maxTokens: maxTokens
-	  })
-	  .then(response => {
-		console.log(response.data.choices[0].text);
-		let responseText = response.data.choices[0].text + "\n";
-
-		const endPos = selectedText.end;
-		const newPosition = endPos.with(endPos.line + 1, 0);
-		
-		editorText.edit(editBuilder => {
-		editBuilder.insert(newPosition, responseText);
-		});
-	  })
-	  .catch(error => {
-		console.error(error);
-	  });	
+function checkApiKeyAvailable() {
+    if (!openaiApiKey) {
+		changeChatGPTApiKey();
+    }
 }
 
-function checkApiKeyAvailable(){
-	if (!openaiApiKey) {
-		// Prompt the user to enter their API key
-		vscode.window.showInputBox({
-			prompt: 'Please set your OpenAI API key',
-			value: openaiApiKey
-		}).then(apiKey => {
-			// Set the API key in the configuration
-			setApiKey(apiKey);
-			return;
-		});
-	}
-}
+function createPrompt(text, additionalPrompt) {
+	return `${basePromptStart}${text}${additionalPrompt}${basePromptEnd}`;
+  }
 
-function setApiKey(apiKey) {
+function setGPTApiKey(apiKey) {
     configuration.update('openai-api-key', apiKey, vscode.ConfigurationTarget.Global)
         .then(() => {
-            // Configuration parameter updated successfully
             vscode.window.showInformationMessage('OpenAI API key has been updated.');
-			openaiApiKey = apiKey;
+            openaiApiKey = apiKey;
         })
         // @ts-ignore
         .catch((error) => {
-            // Error updating configuration parameter
             vscode.window.showErrorMessage('Failed to update OpenAI API key: ' + error.message);
         });
 }
 
-// This method is called when your extension is deactivated
+function changeChatGPTApiKey() {
+	vscode.window.showInputBox({
+		prompt: 'Please set your OpenAI API key',
+		value: openaiApiKey
+	}).then(apiKey => {
+		setGPTApiKey(apiKey);
+		return;
+	});
+}
+
 function deactivate() {}
 
+
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
+
